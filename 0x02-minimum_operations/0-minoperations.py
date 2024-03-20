0167703e164ec5464ef8a49b36b777490d6aaa0d
baydre_africa <baydreafrica@gmail.com>
2024-03-20 21:12:17 +0100

    interview(update): method that calculates the fewest number needed to result n-H characters in a file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,27 +5,29 @@ calculates the fewest number of operations.
 
 single char = H, operations(ops) = copyAll & paste,
 n = number given, result = ?
-
-Solution:
-
-# initialize a variable for the total number of operations
-and single_char to store the current number of H characters
-in the file (initially 1, since we start with one H).
-
-# while single_char is less than n:
-    If n is divisible by H:
-     perform All operations (n // H) times.
-     Update H to be H * (n // H)(since we’ve added more H characters).
-     increment operations by (n // H) (since we performed many operations).
-# return the final value of operations
 """
 
 
-def minOperations(n: int):
+def minOperations(n: int) -> int:
     """
-    returns exactly n-H
-    characters in the file.
+    returns exactly n-H characters in the file.
+
+    Solution:
+
+    # initialize a variable for the total number of operations
+    and single_char to store the current number of H characters
+    in the file (initially 1, since we start with one H).
+
+    # while single_char is less than n:
+    If n is divisible by H:
+       perform All operations (n // H) times.
+       Update H to be H * (n // H)(since we’ve added more H characters).
+       increment operations by (n // H) (since we performed many operations).
+    # return the final value of operations
     """
+    if not isinstance(n, int) or n <= 0:
+        return 0
+
     ops = 0
     single_char = 1
 
